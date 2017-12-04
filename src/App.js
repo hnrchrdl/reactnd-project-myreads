ee4db833b1d2eb4728fb8850f71eb0a0af00d4ac
hnrchrdl <hinrich@posteo.de>
2017-12-04 19:44:24 +0100

    chore: adds react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import * as BooksAPI from './BooksAPI';
 
 import BookShelf from './BookShelf';
+import BrowserRouter from 'react-router-dom';
 import React from 'react';
+import createBrowserHistory from 'history/createBrowserHistory'
 
 class BooksApp extends React.Component {
   state = {
@@ -45,49 +47,51 @@ class BooksApp extends React.Component {
 
   render() {
     return (
-      <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+      <BrowserRouter>
+        <div className="app">
+          {this.state.showSearchPage ? (
+            <div className="search-books">
+              <div className="search-books-bar">
+                <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+                <div className="search-books-input-wrapper">
+                  {/*
+                    NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                    You can find these search terms here:
+                    https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
+                    However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                    you don't find a specific author or title. Every search is limited by search terms.
+                  */}
+                  <input type="text" placeholder="Search by title or author"/>
 
+                </div>
+              </div>
+              <div className="search-books-results">
+                <ol className="books-grid"></ol>
               </div>
             </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
-          <div className="list-books">
-              <div className="list-books-title">
-                <h1>MyReads</h1>
+          ) : (
+            <div className="list-books">
+                <div className="list-books-title">
+                  <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                  {this.state.shelfs.map(shelf => (
+                    <BookShelf 
+                      key={shelf.id}
+                      title={shelf.title}
+                      books={this.state.books.filter(book => book.shelf === shelf.id)}
+                      onBookUpdate={this.updateBookStatus}
+                    />
+                  ))}
               </div>
-              <div className="list-books-content">
-                {this.state.shelfs.map(shelf => (
-                  <BookShelf 
-                    key={shelf.id}
-                    title={shelf.title}
-                    books={this.state.books.filter(book => book.shelf === shelf.id)}
-                    onBookUpdate={this.updateBookStatus}
-                  />
-                ))}
+              <div className="open-search">
+                <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+              </div>
             </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
-        )}
-      </div>
+          )}
+        </div>
+      </BrowserRouter>
     )
   }
 }
